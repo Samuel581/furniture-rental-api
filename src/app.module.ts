5114337d10de7e93558c0ad0a42a5c0ce0565f53
Samuel581 <00023020@uca.edu.sv>
2025-01-08 16:27:57 -0600

    :construction: create(furniture): add base for CRUD on furniture model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FurnitureModule } from './furniture/furniture.module';
 
 @Module({
-  imports: [],
+  imports: [FurnitureModule],
   controllers: [AppController],
   providers: [AppService],
 })
