0cd90497b926fc2755963653e0a147dae0f36bbc
Samuel581 <00023020@uca.edu.sv>
2025-01-08 16:30:26 -0600

    🚧 create(combo) base for CRUD on combo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FurnitureModule } from './furniture/furniture.module';
+import { ComboModule } from './combo/combo.module';
 
 @Module({
-  imports: [FurnitureModule],
+  imports: [FurnitureModule, ComboModule],
   controllers: [AppController],
   providers: [AppService],
 })
