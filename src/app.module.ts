360b9ed29d38486d85c21f6d1516ea4beee609c1
samuel <00023020@uca.edu.sv>
2025-08-20 22:09:53 -0600

    :sparkles: feat(reports) add whole module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { FurnitureModule } from './furniture/furniture.module';
 import { ComboModule } from './combo/combo.module';
 import { RentalModule } from './rental/rental.module';
 import { ClientModule } from './client/client.module';
+import { ReportsModule } from './reports/reports.module';
+import { ReportResolver } from './report/report.resolver';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [ClientModule, FurnitureModule, ComboModule, RentalModule],
+  imports: [ClientModule, FurnitureModule, ComboModule, RentalModule, ReportsModule, ReportModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ReportResolver],
 })
 export class AppModule {}
