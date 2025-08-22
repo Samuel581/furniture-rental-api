3d793159135b9fe157aadc33d3c9b580d7684ae5
samuel <00023020@uca.edu.sv>
2025-08-21 19:56:14 -0600

    :sparkles: feat(rentals) add dateRange helper functions

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,32 +1,80 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
+// Months enum for easy date mamangment
+export enum MONTH { 
+  JANUARY = 1,
+  FEBRUARY = 2,
+  MARCH = 3,
+  APRIL = 4,
+  MAY = 5,
+  JUNE = 6,
+  JULY = 7,
+  AUGUST = 8,
+  SEPTEMBER = 9,
+  OCTOBER = 10,
+  NOVEMBER = 11,
+  DECEMBER = 12
+}
+
+
+interface MonthRange {
+  month: MONTH,
+  monthName: string,
+  startDate: Date,
+  endDate: Date
+}
+
 @Injectable()
 export class ReportService {
 
   constructor(private prisma: PrismaService){}
 
-  private monthsToDateRange(){
+  // Gets the current month range. Use TODO
+  private getCurrentMonthRange(): MonthRange {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
 
+    const startDate = new Date(Date.UTC(year, now.getMonth(), 1, 0, 0, 0, 0))
+    const endDate = new Date(Date.UTC(year, now.getMonth() + 1, 1, 0, 0, 0, 0))
+
+    return {
+      month: month as MONTH,
+      monthName: MONTH[month],
+      startDate,
+      endDate
+    }
+  }
+
+  // Gets the date range for a required month
+  private getMonthRange(month: MONTH, year?: number): MonthRange {
+    const targetYear = year ?? new Date().getFullYear();
+
+    const startDate = new Date(Date.UTC(targetYear, month, 1, 0, 0, 0, 0))
+    const endDate = new Date(Date.UTC(targetYear, month + 1, 1, 0, 0, 0, 0))
+
+    return {
+      month: month as MONTH,
+      monthName: MONTH[month],
+      startDate,
+      endDate
+    }
   }
 
   // Count of rentals per current month
-  totalSumRentals() {
-    return this.prisma.rental.count({})
+  totalCountOfRentals() {
+    
   }
 
   // Total gains per current month
   totalSumGainsRentals() {
     return this.prisma.rental.aggregate({
-      where: {startDate: ""},
+      where: {},
       _sum: {
         totalAmount: true
       }
     })
-  }
-
-  update(id: number, updateReportDto: UpdateReportDto) {
-    return `This action updates a #${id} report`;
   }
 
   remove(id: number) {
