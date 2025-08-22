0b0704d5a5fd833854af8f7048dfe2536777ccbc
samuel <00023020@uca.edu.sv>
2025-08-21 19:55:26 -0600

    🔥 remove(reports) delete unused code/files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,11 @@ import { FurnitureModule } from './furniture/furniture.module';
 import { ComboModule } from './combo/combo.module';
 import { RentalModule } from './rental/rental.module';
 import { ClientModule } from './client/client.module';
-import { ReportsModule } from './reports/reports.module';
-import { ReportResolver } from './report/report.resolver';
 import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [ClientModule, FurnitureModule, ComboModule, RentalModule, ReportsModule, ReportModule],
+  imports: [ClientModule, FurnitureModule, ComboModule, RentalModule, ReportModule],
   controllers: [AppController],
-  providers: [AppService, ReportResolver],
+  providers: [AppService],
 })
 export class AppModule {}
